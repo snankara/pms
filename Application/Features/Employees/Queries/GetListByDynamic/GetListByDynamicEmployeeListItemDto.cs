eb06eebda476d42d9c1187e559292d83e97de254
Sinan Kara <snan_kara_@hotmail.com>
2023-09-06 19:55:57 +0300

    DynamicQuery implemented for Employee entity.

---
+++ Application/Features/Employees/Queries/GetListByDynamic/GetListByDynamicEmployeeListItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Features.Employees.Queries.GetListByDynamic;
+
+public class GetListByDynamicEmployeeListItemDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string TitleName { get; set; }
+    public string DepartmentName { get; set; }
+
+}
