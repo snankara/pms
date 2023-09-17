f818518cce6d4deb5abeb83802c3f8b4650e4d43
Sinan Kara <snan_kara_@hotmail.com>
2023-09-17 12:03:36 +0300

    Business rule added for Title feature.

---
+++ Application/Features/Titles/Rules/TitleBusinessRules.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Titles.Constants;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Titles.Rules;
+
+public class TitleBusinessRules : BaseBusinessRules
+{
+    private readonly ITitleRepository _titleRepository;
+
+    public TitleBusinessRules(ITitleRepository titleRepository)
+    {
+        _titleRepository = titleRepository;
+    }
+
+    public async Task TitleNameCannotBeDuplicatedWhenInserted(string name)
+    {
+        bool result = await _titleRepository.AnyAsync(predicate: t => t.Name.ToLower() == name.ToLower());
+
+        if (result) throw new BusinessException(TitlesMessages.TitleNameExists);
+        
+    }
+}
