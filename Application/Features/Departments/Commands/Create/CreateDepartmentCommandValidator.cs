cfc820bb5bb33617e75b7139969b26f4e0ef3a00
Sinan Kara <snan_kara_@hotmail.com>
2023-09-24 11:36:47 +0300

    Validation rules added for Department domain entity.

---
+++ Application/Features/Departments/Commands/Create/CreateDepartmentCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Departments.Commands.Create;
+
+public class CreateDepartmentCommandValidator : AbstractValidator<CreateDepartmentCommand>
+{
+    public CreateDepartmentCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().MinimumLength(3);
+        RuleFor(c => c.Description).NotEmpty().MinimumLength(5);
+    }
+}
